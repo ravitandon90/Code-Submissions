ac1f682a24d2764d3abc71ffeb7f0696549ad13f
Ayush Gupta <ayushgupta.ac@gmail.com>
2022-08-28 07:39:05 +0530

    1232. Check If It Is a Straight Line

--- Ayush-Gupta/1232. Check If It Is a Straight Line.cpp
+++ Ayush-Gupta/1232. Check If It Is a Straight Line.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool checkStraightLine(vector<vector<int>>& a) {
         int i, n, val, x, y;
-        n=a.size();
-        x=a[1][0] - a[0][0];
-        y=a[1][1] - a[0][1];
+        n = a.size();
+        x = a[1][0] - a[0][0];
+        y = a[1][1] - a[0][1];
         for(i=0; i<n; i++) {
             if((a[i][1] - a[0][1]) * x != (a[i][0] - a[0][0]) * y) return 0;
         }
