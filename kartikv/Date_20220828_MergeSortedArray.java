16b38d0039c9fa9a5062ea36ffbff16cb3604e53
kartech11 <kartech11@gmail.com>
2022-08-29 16:09:32 +0530

    changes to  fiile

--- kartikv/Date_20220828_MergeSortedArray.java
+++ kartikv/Date_20220828_MergeSortedArray.java
@@ -9,14 +9,6 @@
 
   public class Date_20220828_MergeSortedArray {
 
-    public static void main(String[] args) {
-        int[] nums1 = {1, 2, 3, 0, 0, 0};
-        int m = 3;
-        int nums2[] = {2, 5, 6};
-        int n = 3;
-        merge(nums1, m, nums2, n);
-    }
- 
  public static void main(String[] args) {
         int[] nums1 = {1, 2, 3, 0, 0, 0};
         int m = 3;
