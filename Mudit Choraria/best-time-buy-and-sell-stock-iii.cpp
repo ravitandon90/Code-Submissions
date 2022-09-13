7a9ee215e1a14047e392a285c393283d82b0624e
Mudit Choraria <muditchoraria97@gmail.com>
2022-09-13 21:27:59 +0530

    muditchoraria: add solution for best time buy and sell stock iii

---
+++ Mudit Choraria/best-time-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int k = 2;
+        
+        vector<int> buy(k + 1, INT_MAX);
+        vector<int> sell(k + 1, 0);
+        
+        for(auto& price: prices) {
+            for(int i = 1; i <= k; i++) {
+                buy[i] = min(buy[i], price - sell[i - 1]);
+                sell[i] = max(sell[i], price - buy[i]);
+            }
+        }
+        return sell.back();
+    }
+};
