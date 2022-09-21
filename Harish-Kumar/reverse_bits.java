07aa778416838050628c9a4958ba4546ba120416
Harish Kumar R B <harishfalcon400@gmail.com>
2022-09-21 05:52:41 +0530

    Singlw Number solution by harish

--- Harish-Kumar/reverse_bits.java
+++ Harish-Kumar/reverse_bits.java
@@ -2,14 +2,12 @@
 https://leetcode.com/problems/reverse-bits/submissions/
 */
 
-public class Solution {
-    // you need treat n as an unsigned value
-    public int reverseBits(int n) {
-        int result = 0;
-        for (int i = 0; i < 32; i++) {
-            int bit = (n >> i) & 1;
-            result = result | (bit << (31 - i));
-        }
-        return result;
+public int reverseBits(int n) {
+    int res = 0;
+    for(int count = 31 ; count>=0 ; count--){
+        int i = n & 1;
+        res = res * 2 + i;
+        n = n >> 1;
     }
+    return res;
 }
