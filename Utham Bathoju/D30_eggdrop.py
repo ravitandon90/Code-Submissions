5f15bb5c1513a631d5250d1d4b7ffc4d8371b327
Utham Bathoju <uthamkanth@gmail.com>
2022-09-29 15:11:14 +0530

    Update D30_eggdrop.py

--- Utham Bathoju/D30_eggdrop.py
+++ Utham Bathoju/D30_eggdrop.py
@@ -1,21 +1,14 @@
-class Solution {
-    public int twoEggDrop(int floors) {
-        int eggs = 2;
-        int dp[][] = new int[eggs+1][floors+1];
-        int c =0;
+def twoEggDrop(self, n):
+    @lru_cache(None)
+    def dp(n, x):
+        if x == 1 or n <= 1:
+            return n
         
-        for(int i=0; i <= floors; i++){
-            dp[1][i] = i;
-        }
+        res = float("inf")
         
-        for(int f = 1; f <=floors; f++){
-            dp[2][f] = Integer.MAX_VALUE;
-                for(int k = 1; k <=f ; k++){
-                    c = 1 + Math.max(dp[1][k-1], dp[2][f-k]); //the egg breaks on current floor or the egg dosen't break on the current floor. Take the worst case
-                    if(c < dp[2][f])
-                        dp[2][f] = c;
-                }
-            }
-        return dp[eggs][floors];        
-    }
-}
+        for i in range(1,n+1):
+            res = min(res, 1 + max(dp(i-1, x-1), dp(n-i, x)))
+            
+        return res
+    
+    return dp(n, 2)
