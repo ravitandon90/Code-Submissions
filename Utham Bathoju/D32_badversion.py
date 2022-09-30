94c795dbe5adc517a7d823d9d63bed85d6374773
Utham Bathoju <uthamkanth@gmail.com>
2022-09-30 21:57:08 +0530

    Update D32_badversion.py

--- Utham Bathoju/D32_badversion.py
+++ Utham Bathoju/D32_badversion.py
@@ -4,11 +4,11 @@ def firstBadVersion(n):
     :rtype: int
     """
     right = n-1
-    1eft = 0
-    while(1eft<=right):
-        mid = 1eft + (right-1eft)/2
+    left = 0
+    while(left<=right):
+        mid = left + (right-left)/2
         if isBadVersion(mid)==False:
-            1eft = mid+1
+            left = mid+1
         else:
-            right = mid-1eft
-    return 1eft
+            right = mid-left
+    return left
