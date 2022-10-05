f351f36443fad33348861cf6fc7347fd997c6ef6
parth191220 <47950994+parth191220@users.noreply.github.com>
2022-10-05 13:49:24 +0530

    Create maxSubArray.java

---
+++ Parth Mathur/maxSubArray.java
@@ -0,0 +1,18 @@
+class Solution {
+public int maxSubArray(int[] nums) {
+
+    int currsum=0;
+    int maxsum= Integer.MIN_VALUE;
+    if(nums.length==1)
+        return nums[0];
+    for(int i=0;i<nums.length;i++){
+        currsum +=nums[i];
+        if(currsum>maxsum)
+            maxsum=currsum;
+        if(currsum<0){
+            currsum=0;
+        }
+    }
+    return maxsum;
+}
+}
