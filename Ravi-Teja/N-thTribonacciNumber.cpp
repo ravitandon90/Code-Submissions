85fd6c6a21235fa364afa3634fa08a5ae1d013ec
Ravi Teja <66872471+r-teja@users.noreply.github.com>
2022-10-06 12:41:02 +0530

    Added solution to N-th Tribonacci Number

---
+++ Ravi-Teja/N-thTribonacciNumber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0 || n==1){
+            return n;
+        }
+        if(n==2){
+            return 1;
+        }
+        int a=0, b=1, c=1, d;
+        for(int i=3;i<=n;i++){
+            d=a+b+c;
+            a=b;
+            b=c;
+            c=d;
+        }
+        return d;
+    }
+};
