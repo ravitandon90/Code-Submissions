4c8a13603861e0fdc2efec79b63e97f7ca07ddfe
parth191220 <47950994+parth191220@users.noreply.github.com>
2022-10-14 10:14:16 +0530

    Create 59_findTargetSumWays.java

---
+++ Parth Mathur/59_findTargetSumWays.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+         int sum = 0;
+        for(int x : nums)
+            sum += x;
+        if(((sum - target) % 2 == 1) || (target > sum))
+            return 0;
+        
+        int n = nums.length;
+        int s2 = (sum - target)/2;
+        int[][] t = new int[n + 1][s2 + 1];
+        t[0][0] = 1;
+        
+        for(int i = 1; i < n + 1; i++) {
+            for(int j = 0; j < s2 + 1; j++) {
+                if(nums[i - 1] <= j)
+                    t[i][j] = t[i-1][j] + t[i - 1][j - nums[i - 1]];
+                else
+                    t[i][j] = t[i - 1][j];
+            }
+        }
+        return t[n][s2];
+
+    }
+}
