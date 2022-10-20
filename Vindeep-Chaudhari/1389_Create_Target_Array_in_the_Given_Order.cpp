bda93e073943ec0cb6ef80b3c326c4e6788b41af
vindeep07 <vindeepchaudhari@gmail.com>
2022-10-20 22:24:16 +0530

    1389. Create Target Array in the Given Order

---
+++ Vindeep-Chaudhari/1389_Create_Target_Array_in_the_Given_Order.cpp
@@ -0,0 +1,16 @@
+/**
+ * Problem Link - https://leetcode.com/problems/number-of-provinces/
+ */
+
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        
+        vector<int> target ;
+        for(int i = 0; i < nums.size(); i++) {
+            target.insert(target.begin()+index[i], nums[i]);
+        }
+        
+        return target;
+    }
+};
