3d9adcf2f1bda85071cb61e75c950084d2054e45
Dhairyashil Shinde <93669429+dhairyashiil@users.noreply.github.com>
2022-11-14 01:05:02 +0530

    Update 90. Find all collection of four elements that have sum equal to K.cpp

--- Dhairyashil-Shinde/90. Find all collection of four elements that have sum equal to K.cpp
+++ Dhairyashil-Shinde/90. Find all collection of four elements that have sum equal to K.cpp
@@ -1,21 +1,65 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int T) {
-        unordered_set<int> seen;
-        set<multiset<int>> ans_set;
-        int n = size(nums);
-        for(int i = 0; i < n; i++) {
-            for(int j = i+1; j < n; j++) {
-                for(int k = j+1; k < n; k++) {
-                    int need = T - (nums[i] + nums[j] + nums[k]);
-                    if(seen.count(need)) 
-                        ans_set.emplace(multiset<int>{nums[i], nums[j], nums[k], need});
-                }                
+    vector<vector<int>> fourSum(vector<int>& nums, int target)
+     {
+
+      vector<vector<int>> v;
+      sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size(); i++){
+        if(i != 0 && nums[i] == nums[i - 1]) continue;
+        for(int j = i + 1; j < nums.size(); j++)
+        {
+          if(j != i + 1 && nums[j] == nums[j - 1]) continue;
+          int k = j + 1;
+          int l = nums.size() - 1;
+          while(k < l){
+            long long int sum = (long long) nums[i] + (long long) nums[j] + (long long) nums[k] + (long long) nums[l];
+            if(target - sum == 0)
+            {
+              v.push_back({nums[i], nums[j], nums[k], nums[l]});
+              k++;
+              while(k < l && nums[k] == nums[k - 1]) k++;
             }
-            seen.insert(nums[i]);
+            else if(sum < target) k++;
+            else l--;
+          }
+          
         }
-        vector<vector<int>>ans(size(ans_set));
-        for_each(begin(ans_set), end(ans_set), [&, i(0)](auto& el) mutable { ans[i++] = vector<int>(begin(el), end(el)); });
-        return ans;
+      }
+      
+      return v;
+}
+};
+/*
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        
+        vector<vector<int>> ans;
+        vector<int> quadruplets;
+        sort(nums.begin(), nums.end());
+        int k = 4;
+        int sum = 0;
+
+        int n = nums.size();
+        for(int i=0; i<n; i++) {
+
+            int j=i;
+            while(j<k) {
+                sum += nums[j];
+                j++;
+            }
+
+            if(sum == target) {
+                for(int p=i; p<k; p++) {
+                    quadruplets.push_back(nums[i]);
+                }
+                
+            }
+        }
+
     }
 };
+
+*/
