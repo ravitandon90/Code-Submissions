3fab4c34ed8583d8975a714a8391cb6ccb78eb52
Mudit Choraria <32506556+muditchoraria@users.noreply.github.com>
2022-11-17 21:53:53 +0530

    muditchoraria: add soln for rect area

---
+++ Mudit Choraria/rect-area.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, 
+                    int bx1, int by1, int bx2, int by2) {
+        int x = max(ax1, bx1) < min(ax2, bx2) ? (min(ax2, bx2) - max(ax1, bx1)) : 0;
+        int y = max(ay1, by1) < min(ay2, by2) ? (min(ay2, by2) - max(ay1, by1)) : 0;
+        return (ax2 - ax1) * (ay2 - ay1) + (bx2 - bx1) * (by2 - by1) - x * y;
+    }
+};
